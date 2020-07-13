61ff3e6923bbda5d139690761075ff517f349049
marah <marahabdallah91@gmail.com>
2020-07-13 19:08:40 +0300

    lab36

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -1,45 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { showAction } from '../../store/actions';
+import {Breadcrumbs, Link,Container,Typography} from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const Categories = (props) => {
   return (
-    <section>
-      <h3>Categories:</h3>
-      <ul>
-        {props.actions.categories.map((category , i) => {
-          return (
-            <li key={i} onClick={() => props.showAction(category.name) }>
-              {category.displayName}
-            </li>
-          );
-        })}
-      </ul>
+    <>
+      <CssBaseline />
 
-      <p>Active category : { props.actions.activeCategory}</p>
+      <Container maxWidth="sm">
+        <Typography component="div" style={{ backgroundColor: '#cae3d3', height: '30vh' ,padding: '3vh' }} >
+        
+          <Typography variant="h4" gutterBottom>
+        Categories:
+          </Typography>
 
-    </section>
+          <Breadcrumbs aria-label="breadcrumb" >
+            {props.actions.categories.map((category,i) => {
+              return (
+                <Link key={i} color="inherit" onClick={() => props.showAction(category.name)}>
+                  {category.displayName}
+                </Link>
+              );
+            })}
+          </Breadcrumbs>
+
+          <Typography variant="overline" display="block" gutterBottom>
+        Active category : { props.actions.activeCategory}
+          </Typography>
+        
+        </Typography>
+      </Container>
+    </>
   );
 };
-  
+ 
+
+
+
+//mapping state to props will subscribe the the change to the state and rerender :  
+// props.actions.categories
+// props.actions.activeCategory
 const mapStateToProps = (state) => {
   return { actions: state.actions };
 };
-  // props.actions.categories
-  // props.actions.activeCategory
+  
 
+// Dispatches an action when one is clicked to “activate” it
+const mapDispatchToProps = { showAction };  // props.showAction
 
-const mapDispatchToProps = { showAction };  // props.categories
-/*categoriesAction
-  function mapDispatchToProps(dispatch){
-    return(
-      {
-        categories:()=>{dispatch(categories())},
-         products:()=>{dispatch(products())}
-      }
-    )
-  }
-  */
 
 export default connect(mapStateToProps, mapDispatchToProps)(Categories);
   
