cd2ab5fa1ef83bd952755cfa65fe51f1b8fe7433
marah <marahabdallah91@gmail.com>
2020-07-16 02:14:41 +0300

    thunk

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,5 +1,6 @@
 let initialState = {
   carts: [],
+  totalCartItems: 0,
 };
 
 export default (state = initialState, action) => {
@@ -11,12 +12,12 @@ export default (state = initialState, action) => {
   case 'ADD PRODUCT':
     carts = state.carts;
     carts.push(payload);
-    return { carts };
+    return { carts ,totalCartItems: ++state.totalCartItems};
   
   case 'REMOVE PRODUCT':
     carts = state.carts;
     carts.splice(carts.indexOf(payload), 1);
-    return { carts };
+    return { carts ,totalCartItems: --state.totalCartItems};
   
   default:
     return state;
